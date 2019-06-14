76d45d00e0e99afb88dcc4550ed5930a48758aba
jamievincent <jamievincent99"gmail.com>
2019-06-14 18:08:30 +1000

    s

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import json
 from .models import *
+import os, sys 
 
 # Create your views here.
 
@@ -34,3 +35,36 @@ def example_post(request):
 			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
 	else:
 		return HttpResponse("ONLY POST REQUESTS")
+
+@csrf_exempt
+def fib(request):
+	jsob = {'startNumber':'0', 'length': 10}
+	log = []
+	if request.method == "POST":
+		try:
+			data = request.POST["data"]
+			received = json.loads(data)
+			jsob.update(received)
+
+			startNumber = int(jsob["startNumber"]) # converts start number into integer
+			length = int(jsob["length"]) # converts length into integer
+			loop = range(length) # creates a range of numbers
+			
+			numarray = []
+			fibno = startNumber
+			addno = 1
+			for l in loop:
+				numarray.append(fibno)
+				fibno = fibno + addno
+				addno = fibno - addno
+
+			return JsonResponse({"fib":numarray})
+
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+	else:
+		return JsonResponse(jsob)
