3204a28b370950d64c57a58914a5c8aad3797c6d
jamievincent <jamievincent99"gmail.com>
2019-07-25 09:36:53 +1000

    a

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -33,6 +33,21 @@ def example_get(request, var_a, var_b):
 		errorType = str(exc_type)
 		return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
 
+def count(request, start, stop, step):
+	try:
+		count = 0
+    	count_list = []
+    	for count in range(start,stop,step):
+    		count_list.append(count)
+		return JsonResponse(count_list)
+	except Exception as e:
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		other = sys.exc_info()[0].__name__
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+		errorType = str(exc_type)
+		return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+
+
 @csrf_exempt
 def example_post(request):
 	log = []
