a7460eea435d00961d3e2621e9dcc3b2e63bd275
jamievincent <jamievincent99"gmail.com>
2019-07-25 10:39:55 +1000

    a

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'example/', include('example.urls')),
-    path(r'c/', include('cekyl.urls')),
-
+    path(r'cekyl/', include('cekyl.urls')),
 ]
