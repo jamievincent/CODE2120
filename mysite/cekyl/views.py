15048be9234c658ea35c8c49a2709eb4853c6d29
jamievincent <jamievincent99"gmail.com>
2019-07-30 11:05:13 +1000

    a

--- mysite/cekyl/views.py
+++ mysite/cekyl/views.py
@@ -7,11 +7,11 @@ import os, sys
 
 # Create your views here.
 def count(request, var_a, var_b):
-	var_a = start
-	var_b = step
-	stop = 100
-	count_list = []
 	try:
+		var_a = start
+		var_b = step
+		stop = 100
+		count_list = []
 		while(start<stop):
 			count_list.append(start)
 			start = start + step
