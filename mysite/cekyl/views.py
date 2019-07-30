9b93f45553a05a5097edaf5065c089f7254c4347
jamievincent <jamievincent99"gmail.com>
2019-07-30 10:59:24 +1000

    a

--- mysite/cekyl/views.py
+++ mysite/cekyl/views.py
@@ -10,16 +10,16 @@ def count(request, var_a, var_b):
 	var_a = start
 	var_b = step
 	stop = 100
-    count_list = []
+	count_list = []
 	try:
-    	while(start<stop):
-        	count_list.append(start)
-       	 	start = start + step
-    return count_list
-		return JsonResponse(count_list)
-	except Exception as e:
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		other = sys.exc_info()[0].__name__
-		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		errorType = str(exc_type)
-		return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+		while(start<stop):
+			count_list.append(start)
+			start = start + step
+			return count_list
+			return JsonResponse(count_list)
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
